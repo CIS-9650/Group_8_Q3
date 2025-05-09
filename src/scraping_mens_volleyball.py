10e7b10493ef65704ce0ca5ecb54f6446791108e
Jason Riffaterre <jason.riffaterre@baruchmail.cuny.edu>
2025-05-09 09:49:56 -0400

    created a new file and branch for my segment of the team 8 task scraping mens' volleyball team heights code

---
+++ src/scraping_mens_volleyball.py
@@ -0,0 +1,9 @@
+# scraping_mens_volleyball.py
+
+# this is where the portion of code to scrape the heights of
+# the mens' volleyball teams is being developed
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
